8d12101da9ec1da309353daee6d612c8f3ef78c3
Maciej Matuszczak <maciej.matuszczak96@gmail.com>
2018-11-06 01:10:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# BlogPort
+
+**Instalation**
+
+    git clone https://github.com/Maciek246/blogport.git
+    cd blogport
+    # yarn
+    yarn add
+    
+    # npm
+    # npm install 
---
+++ index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
